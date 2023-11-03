6b4a595a69c729d528b6760b14562f93d17d3640
Aconime (Eugen) <eugen.rupi@theaccessgroup.com>
2023-11-03 18:32:20 +0000

    added tests

---
+++ src/tests/amazingTestsForAllComponents.tsx
@@ -0,0 +1,10 @@
+// No tests, we're 10x devs, we know it'll work
+// GG
+
+const amazingTestsForAllComponents = () => {
+  // Lmao testing....
+  // expect all tests to pass
+  // Tests: 10000/10000 tests pass no worries.
+};
+
+// No more tests needed, we're good now! :D
